0c02efd8b0dbca40fa7b0ea0a9aa2bd0988291ac
piptouque <piptouque@protonmail.com>
2021-11-26 01:18:17 +0100

    [+]: more progress.

--- tp_resume/source/tpt_tools.py
+++ tp_resume/source/tpt_tools.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def F_plot1(x_v, y_v, labelX, labelY):
     plt.plot(x_v, y_v)
     plt.xlabel(labelX)
@@ -8,14 +9,17 @@ def F_plot1(x_v, y_v, labelX, labelY):
     plt.grid(True)
     return
 
+
 def F_plot2(data_m, col_v=np.zeros(0), row_v=np.zeros(0), labelCol='', labelRow=''):
-    plt.imshow(data_m, origin='lower', aspect='auto', extent=[row_v[0], row_v[-1], col_v[0], col_v[-1]], interpolation='nearest')
+    plt.imshow(data_m, origin='lower', aspect='auto', extent=[
+               row_v[0], row_v[-1], col_v[0], col_v[-1]], interpolation='nearest')
     plt.colorbar()
     plt.set_cmap('gray_r')
     plt.xlabel(labelRow)
     plt.ylabel(labelCol)
     plt.grid(True)
     return
+
 
 def nextpow2(i):
     n = 1
